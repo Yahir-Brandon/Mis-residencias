e5d1ec70d495a89b7d9103891f2ba0ea1322b523
Brandon Gonzalez <yahirbran16@gmail.com>
2025-11-24 05:33:58 +0000

    ajusta bien el tamaño de letra usa letra formal y disminuye el tamaño de

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,8 +17,8 @@ export default {
     },
     extend: {
       fontFamily: {
-        body: ['PT Sans', 'sans-serif'],
-        headline: ['PT Sans', 'sans-serif'],
+        body: ['Arial', 'sans-serif'],
+        headline: ['Arial', 'sans-serif'],
         code: ['monospace'],
       },
       colors: {
